a8f7cb6e43bc71f1aae884d3b0083b15d2a7bf3e
jxLiu <2581545272@qq.com>
2022-07-27 18:59:21 +0800

    Conflict In Master Test

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("Hello World!");
         System.out.println("Hello World! 2");
         System.out.println("In hot-fix");
+        System.out.println("In master Conflict!");
     }
 }
